53a7e9861afa908f8006262d2f5cc3c3b013c514
Maliyagithub <129000805+Maliyagithub@users.noreply.github.com>
2023-03-27 13:39:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,70 @@
-import os
-import telebot
+from telegram import *
+
+from telegram import update
+
+from telegram.ext import *
+
+from telegram.ext import updater
+
+from telegram.ext import dispatcher
+
+from telegram.ext import commandhandler
+
+from telegram.ext import callbackcontext
 
 
-bot = telebot.TeleBot("6170513427:AAG_F2nLSV7a3MtupqWcwWgV-S9kpSHScAk")
-
-
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm maliya Bro Chat Bot")
-
-
-@bot.message_handler(commands=["how"])
-def send_message(message):
-  bot.send_message(message.chat.id, "https://drive.google.com/uc?export=download&id=1BJfGsK-2iPf4wfTScjF5zB0qN3k0fZM2")
 
 
 
-bot.polling()
+bot = Bot("5953671984:AAG9XaarXNDyM0axBHZVTLjtnjwQ6bpIcTE")
+
+#print(bot.get_me())
+
+updater=Updater("5953671984:AAG9XaarXNDyM0axBHZVTLjtnjwQ6bpIcTE",use_context=True)
+
+
+
+dispatcher=updater.dispatcher
+
+
+
+def test_function(update:update,context:CallbackContext):
+
+    bot.send_message(
+
+        chat_id=update.effective_chat.id,
+
+        text="Hello I'm Maliyavpn Bot "
+
+        )
+
+start_value=CommandHandler('start',test_function) 
+
+
+
+dispatcher.add_handler(start_value)
+
+
+
+def test_function(update:update,context:CallbackContext):
+
+    bot.send_message(
+
+        chat_id=update.effective_chat.id,
+
+        text="This is my bot "
+
+        )
+
+help_value=CommandHandler('help',test_function) 
+
+
+
+dispatcher.add_handler(help_value)
+
+
+
+updater.start_polling()
+
+
+
